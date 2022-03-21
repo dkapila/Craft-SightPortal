87195f77902fd20da103eedb49f064199adf7e29
Dharam Kapila <dharamkapila@gmail.com>
2022-03-21 17:15:19 +0100

    Update version number

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,7 +9,7 @@ const Constants = {
   // Links
   ABOUT_ME_LINK: 'https://dharam.is',
   SIGHT_PORTAL_DOWNLOAD_LINK: 'https://dharam.is/Sight-Portal-Download',
-  SIGHT_PORTAL_UPDATE_STATUS_LINK: 'https://sightportal.dharamkapila.repl.co/Versions/0.3/0.3beta.html',
+  SIGHT_PORTAL_UPDATE_STATUS_LINK: 'https://sightportal.dharamkapila.repl.co/Versions/0.4/0.4beta.html',
   SIGHT_PORTAL_UPDATE_HEADER_STATUS_LINK: 'https://sightportal.dharamkapila.repl.co/Versions/0.4/0.4betaHeader.html',
   MAIL_TO_LINK: 'mailto:dharam@hey.com',
   TWITTER_LINK: 'https://www.twitter.com/dharamkapila',
